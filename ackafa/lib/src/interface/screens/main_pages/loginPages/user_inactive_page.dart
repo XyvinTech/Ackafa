505855433b4bd7110e6b8235259c35e4f4c9d0e3
sreeramis8888 <128227002+sreeramis8888@users.noreply.github.com>
2024-09-12 20:40:50 +0530

    .

--- ackafa/lib/src/interface/screens/main_pages/loginPages/user_inactive_page.dart
+++ ackafa/lib/src/interface/screens/main_pages/loginPages/user_inactive_page.dart
@@ -1,77 +1,61 @@
-import 'dart:async';
-
 import 'package:ackaf/src/data/providers/user_provider.dart';
 import 'package:ackaf/src/interface/screens/main_pages/loginPages/paymentpage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-class UserInactivePage extends ConsumerStatefulWidget {
+class UserInactivePage extends ConsumerWidget {
   const UserInactivePage({super.key});
 
-  @override
-  _UserInactivePageState createState() => _UserInactivePageState();
-}
-
-class _UserInactivePageState extends ConsumerState<UserInactivePage> {
-  Timer? _timer;
-
-  @override
-  void initState() {
-    super.initState();
-    _startPeriodicRefresh();
-  }
-
-  void _startPeriodicRefresh() async {
-    _timer = Timer.periodic(const Duration(seconds: 5), (timer) async {
-      final user = await ref.read(userProvider.notifier).refreshUser();
-      if (user?.status == 'awaiting_payment') {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => PaymentConfirmationPage()),
-        );
-      }
-    });
+  Future<void> _refreshUser(WidgetRef ref, BuildContext context) async {
+    final user = await ref.read(userProvider.notifier).refreshUser();
+    if (user?.status == 'awaiting_payment') {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => PaymentConfirmationPage()),
+      );
+    }
   }
 
   @override
-  void dispose() {
-    _timer?.cancel(); // Cancel the timer when the widget is disposed
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: RefreshIndicator(
         backgroundColor: Colors.white,
         color: Colors.red,
-        onRefresh: () async => ref.invalidate(userProvider),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        onRefresh: () async => _refreshUser(ref, context),
+        child: ListView(
           children: [
-            LoadingAnimationWidget.hexagonDots(
-              color: const Color(0xFFE30613),
-              size: 80,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Your request to join has been sent',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Access to the app will be made',
-              style: TextStyle(fontSize: 15, color: Colors.grey),
-            ),
-            const Text(
-              'available to you soon',
-              style: TextStyle(fontSize: 15, color: Colors.grey),
-            ),
-            const Text(
-              'Thank you for your patience',
-              style: TextStyle(fontSize: 15, color: Colors.grey),
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  LoadingAnimationWidget.hexagonDots(
+                    color: const Color(0xFFE30613),
+                    size: 80,
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Your request to join has been sent',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Access to the app will be made',
+                    style: TextStyle(fontSize: 15, color: Colors.grey),
+                  ),
+                  const Text(
+                    'available to you soon',
+                    style: TextStyle(fontSize: 15, color: Colors.grey),
+                  ),
+                  const Text(
+                    'Thank you for your patience',
+                    style: TextStyle(fontSize: 15, color: Colors.grey),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
