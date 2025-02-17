eb4f5c6bde3cfbc1f0da4800cf4e0d3ee564fc82
sreeramis8888 <128227002+sreeramis8888@users.noreply.github.com>
2025-02-17 10:38:46 +0530

    git stash

--- ackafa/lib/src/interface/screens/main_pages/profilePage.dart
+++ ackafa/lib/src/interface/screens/main_pages/profilePage.dart
@@ -128,6 +128,12 @@ class ProfilePage extends StatelessWidget {
                                               ],
                                             ),
                                           ),
+                         ),
+                                          ),
+                         ),
+                                          ),
+                         ),
+                                          ),
                                         ],
                                       ),
                                       SizedBox(
